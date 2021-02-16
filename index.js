223e52e6b79024c97eb684f140f0c843c2126aca
Abdullah <abdullahoguk@gmail.com>
2021-02-16 17:22:39 +0300

    write to file

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const {SITELIST} = require("./urls.js");
+const { SITELIST } = require("./urls.js");
 const fs = require("fs");
 const { parse } = require('node-html-parser');
-const {fetchAsync} = require('./utils.js');
+const { fetchAsync } = require('./utils.js');
 const BASEURL = "https://www.alexa.com/siteinfo/"
-const trimmedSiteList = SITELIST.map(_=>_.split("/")[2]).map(_=>_.startsWith("www") ? _.slice(4):_)
+const trimmedSiteList = SITELIST.map(_ => _.split("/")[2]).map(_ => _.startsWith("www") ? _.slice(4) : _)
+
 start();
 
- async function start(){
-    var stats=[];
+async function start() {
+    var data = {};
+    var stats = [];
 
-
-    for(site of trimmedSiteList){ 
-    var fullRawHTML = await fetchAsync(BASEURL+site);
-    const fullDOM = parse(fullRawHTML);
-    stats.push(scrapeData(fullDOM,site))
+    for (site of trimmedSiteList) {
+        var fullRawHTML = await fetchAsync(BASEURL + site);
+        const fullDOM = parse(fullRawHTML);
+        stats.push(scrapeData(fullDOM, site))
     }
-    console.log(stats)
 
+    data.stats = stats;
+    data.lastUpdated = new Date().toLocaleString('tr-TR', { day:'numeric', month:'long', year:"numeric",hour:"numeric",minute:"numeric",hour12: false ,timeZone: 'Asia/Istanbul' });
+    
+    fs.writeFile("data.json", JSON.stringify(data), () => {
+        console.log("data.json yazıldı");
+    });
+}
 
- }
-
- function scrapeData(fullDOM,site){
-
+function scrapeData(fullDOM, site) {
     var siteData = {};
     siteData.url = site;
-    siteData.globalRank=fullDOM.querySelector(".rank-global .big.data") ? fullDOM.querySelector(".rank-global .big.data").innerText.split("#")[1].trim() : null;
-    siteData.turkeyRank=fullDOM.querySelector("#countrydropdown .pull-right") ? fullDOM.querySelector("#countrydropdown .pull-right").innerText.split("#")[1].trim() : null;
-    console.log(siteData)
-
+    siteData.globalRank = fullDOM.querySelector(".rank-global .big.data") ? fullDOM.querySelector(".rank-global .big.data").innerText.split("#")[1].trim() : null;
+    siteData.turkeyRank = fullDOM.querySelector("#countrydropdown .pull-right") ? fullDOM.querySelector("#countrydropdown .pull-right").innerText.split("#")[1].trim() : null;
+    console.log(site);
     return siteData;
-
- }
+}
